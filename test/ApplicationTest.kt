3b38345d3e8c164ea5093c7d7baa7c8e88e66822
saloed <napster_1997@mail.ru>
2019-05-30 22:07:46 +0300

    start test

--- test/ApplicationTest.kt
+++ test/ApplicationTest.kt
@@ -1,29 +1,109 @@
 package ru.spbstu.architectures
 
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
-import io.ktor.routing.*
-import io.ktor.http.*
-import io.ktor.locations.*
-import io.ktor.sessions.*
-import io.ktor.features.*
-import org.slf4j.event.*
-import io.ktor.http.content.*
-import io.ktor.util.date.*
-import io.ktor.auth.*
-import io.ktor.gson.*
-import kotlin.test.*
+import com.google.gson.Gson
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpMethod
 import io.ktor.server.testing.*
+import io.ktor.server.testing.handleRequest
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.mockkObject
+import io.mockk.unmockkAll
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+import ru.spbstu.architectures.pizzaService.db.manager.UserModelManager
+import ru.spbstu.architectures.pizzaService.models.*
+import ru.spbstu.architectures.pizzaService.utils.Hasher
+import ru.spbstu.architectures.pizzaService.web.UserCredentials
+import kotlin.test.Test
+import kotlin.test.assertFails
+import kotlin.test.assertFalse
+import kotlin.test.assertNotNull
+
+val gson = Gson()
 
 class ApplicationTest {
-    @Test
-    fun testRoot() {
-        withTestApplication({ module(testing = true) }) {
-            handleRequest(HttpMethod.Get, "/").apply {
-                assertEquals(HttpStatusCode.OK, response.status())
-                assertEquals("HELLO WORLD!", response.content)
-            }
+
+    val password = "password"
+
+
+    private val debugUsers = mapOf<String, User>(
+        "client" to Client(11, "client", password, "address", "phone"),
+        "client2" to Client(12, "client2", password, "address2", "phone2"),
+        "manager" to Manager(21, "manager", password, "restaurant"),
+        "operator" to Operator(31, "operator", password, 1),
+        "courier" to Courier(41, "courier", password)
+    )
+
+    private val userTokens = mutableMapOf<User, Token>()
+
+    @Before
+    fun beforeTests() {
+        mockkObject(Hasher)
+        every { Hasher.hash(password) } returns password
+        mockkObject(UserModelManager)
+        for ((login, user) in debugUsers) {
+            coEvery { UserModelManager.get(login, password) } returns user
+            coEvery { UserModelManager.get(login) } returns user
         }
     }
+
+    fun TestApplicationEngine.authenticate(user: User) = userTokens.computeIfAbsent(user) {
+        val credentials = UserCredentials(user.login, user.password)
+        handleRequest(HttpMethod.Post, "/authenticate") {
+            setJsonBody(credentials)
+        }.getJson()
+    }
+
+    fun TestApplicationEngine.handleRequestWithUser(
+        method: HttpMethod,
+        uri: String,
+        user: User,
+        setup: TestApplicationRequest.() -> Unit = {}
+    ): TestApplicationCall = handleRequest {
+        this.uri = uri
+        this.method = method
+        val token = authenticate(user)
+        addHeader(HttpHeaders.Authorization, "Bearer ${token.token}")
+        setup()
+    }
+
+
+    @Test
+    fun testLoginSuccessWithTracker() = testApp {
+        val hash = Hasher.hash(password)
+        val user = User.manager.get("client", hash)!!
+
+        val response = handleRequestWithUser(HttpMethod.Get, "/pizza", user).getJson<List<Pizza>>()
+
+
+        println("$response")
+        println("$user $hash")
+    }
+
+
+    private fun testApp(callback: suspend TestApplicationEngine.() -> Unit) {
+        withTestApplication({ module() }) { runBlocking { callback() } }
+    }
+
+    @After
+    fun afterTests() {
+        unmockkAll()
+    }
+
+}
+
+inline fun <reified T> TestApplicationRequest.setJsonBody(value: T) {
+    val data = gson.toJson(value)
+    setBody(data.toByteArray())
+    addHeader(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+}
+
+
+inline fun <reified T> TestApplicationCall.getJson(): T {
+    println("$this")
+    val value = assertNotNull(response.content, "response content is null")
+    return gson.fromJson<T>(value, T::class.java)
 }
